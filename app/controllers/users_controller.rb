1a73e4fbacbf3b29a6d72da97af72f5d45614f82
rfenix3 <richard.fenix@gmail.com>
2018-09-16 22:09:14 -0700

    Create root, user sign-in, and sign-out views and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    byebug
     @user = User.new(user_params)
     if @user.save
-      # session[:user_id] = @user.id
-      byebug
+      session[:user_id] = @user.id
       redirect_to user_path(@user), notice: "Welcome to Issue Manager!"
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root "static_pages#home"
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+
   resources :users
   resources :issues
   resources :employees
