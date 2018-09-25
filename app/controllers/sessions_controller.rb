d04568162de828a5ccdab126135861389e77afac
rfenix3 <richard.fenix@gmail.com>
2018-09-25 10:41:06 -0700

    Add login validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,35 +13,27 @@ class SessionsController < ApplicationController
   def create
     if request.env["omniauth.auth"]
       # They login via OAuth
-      byebug
       oauth_email = request.env["omniauth.auth"]["info"]["nickname"]
       if @user = User.find_by(:name => oauth_email)
-        byebug
         session[:user_id] = @user.id
         redirect_to user_path(@user), notice: "Welcome back to issue manager!"
       else
         # Default password is set to 'guest' for Github user
-        byebug
         @user = User.new(:name => oauth_email, :password => "guest", :role => "Guest")
         if @user.save
-          byebug
           session[:user_id] = @user.id
           redirect_to user_path(@user), notice: "Welcome back to issue manager!"
         else
-          byebug
           redirect_to signin_path
         end
       end
     else
-      # Normal login with user name and password
+      # Normal login with user name and password    
       @user = User.find_by(name: params[:user][:name])
-      #byebug
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        #byebug
         redirect_to user_path(@user), notice: "Welcome back to issue manager!"
       else
-        #byebug
         @user.errors.add(:password, "Username and Password must match or exist")
         render template: "sessions/new"
         #redirect_to sessions_new_path
