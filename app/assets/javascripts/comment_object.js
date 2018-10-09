b03f400906a167b9060198a5d2f842e5aecefb51
rfenix3 <richard.fenix@gmail.com>
2018-10-08 22:35:15 -0700

    Convert comment function to js model object and add constructor to display new comment message

--- app/assets/javascripts/comment_object.js
+++ app/assets/javascripts/comment_object.js
@@ -1,13 +1,25 @@
-function Comment(id, comment, issue_id, user_id, created_at) {
-  this.id = id;
-  this.comment = comment;
-  this.issue_id = issue_id;
-  this.user_id = user_id;
-  this.created_at = created_at;
-}
+class Comment {
+  constructor(id, comment, issue_id, user_id, created_at) {
+    this.id = id;
+    this.comment = comment;
+    this.issue_id = issue_id;
+    this.user_id = user_id;
+    this.created_at = created_at;
+  };
 
-Comment.prototype.getUserName = function() {
-  console.log(`Hello everybody!`);
-};
+  formatCreateDate() {
+    var create_date = this.created_at;
+
+    var month = create_date.slice(5,7);
+    var day = create_date.slice(8,10);
+    var year = create_date.slice(0,4);
+    var time = create_date.slice(11,19)
+    
+    var formatted_date = `${month}/${day}/${year} ${time} <--- New Comment!`;
+    
+    return formatted_date;
+  };
+
+}
 
 
